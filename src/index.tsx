9a1ecb8f17874b6aea128e489ab1c366a18d8e6e
amirbagjni <amirbagjani@gmail.com>
2022-07-23 19:45:07 +0430

    ADD libraries, plugin and loader

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+//createRoot is available on react-dom/client
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<App />);
